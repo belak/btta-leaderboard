94bdbf5d8a3c16de50d6a13349d9453476da68cd
Kaleb Elwert <belak@coded.io>
2020-05-23 18:13:11 -0700

    Updates

---
+++ precache-manifest.fffc316b090d12c37092b760e486468f.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27f47d7882b1a9243e619b0622d3c9f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6c619a1237b3677d539",
+    "url": "/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "44481c449ea866a3ebc1",
+    "url": "/static/css/main.00810bb6.chunk.css"
+  },
+  {
+    "revision": "a6c619a1237b3677d539",
+    "url": "/static/js/2.351fd7a0.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/static/js/2.351fd7a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44481c449ea866a3ebc1",
+    "url": "/static/js/main.eba4260e.chunk.js"
+  },
+  {
+    "revision": "ec0b314efaa16e4ed8bb",
+    "url": "/static/js/runtime-main.16c8658c.js"
+  },
+  {
+    "revision": "058d084e3b4b740e8bedf15d4b82d63d",
+    "url": "/static/media/bitwise.058d084e.svg"
+  },
+  {
+    "revision": "06ffefa81ce6b604fa86b957cd67487d",
+    "url": "/static/media/bitwise.06ffefa8.woff"
+  },
+  {
+    "revision": "10424485c0158204d026d1dcee8dca4f",
+    "url": "/static/media/bitwise.10424485.woff2"
+  },
+  {
+    "revision": "cdc2d155ed39fa4e415c48bc2f965715",
+    "url": "/static/media/bitwise.cdc2d155.ttf"
+  },
+  {
+    "revision": "f613946256bc16d3689f12589a3425bf",
+    "url": "/static/media/bitwise.f6139462.eot"
+  }
+]);
